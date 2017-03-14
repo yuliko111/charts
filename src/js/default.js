8f1336304635bcd7397b8c7e226770eb77151c88
yuliko <yuliko111@mail.ru>
2017-03-14 14:12:04 +0300

    add area and gradient!

--- src/js/default.js
+++ src/js/default.js
@@ -1,52 +1,114 @@
 	// import * as d3 from 'd3';
 (function () {
-	var svg = d3.select("svg"),
-		margin = {top: 20, right: 20, bottom: 30, left: 50},
-		width = +svg.attr("width") - margin.left - margin.right,
-		height = +svg.attr("height") - margin.top - margin.bottom,
-		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var margin = {top: 20, right: 100, bottom: 30, left: 100},
+		width = 960 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
 
-	var parseTime = d3.timeParse("%d-%b-%y");
+	var dataset = [
+		{x: 0, y: 0},
+		{x: 0, y: 5},
+		{x: 1, y: 8},
+		{x: 2, y: 13},
+		{x: 3, y: 12},
+		{x: 4, y: 16},
+		{x: 5, y: 21},
+		{x: 6, y: 18},
+		{x: 7, y: 23},
+		{x: 8, y: 24},
+		{x: 9, y: 28},
+		{x: 10, y: 35},
+		{x: 11, y: 30},
+		{x: 12, y: 32},
+		{x: 13, y: 36},
+		{x: 14, y: 40},
+		{x: 15, y: 38},
+		{x: 15, y: 0},
+	];
 
-	var x = d3.scaleTime()
-		.rangeRound([0, width]);
+	// возвращасет значение примерно в 3 раза больше (для X)
 
-	var y = d3.scaleLinear()
-		.rangeRound([height, 0]);
+	var xScale = d3.scale.linear()
+		.domain([0, d3.max(dataset, function (d) {
+			return d.x;
+		})])
+		.range([0, width]);
 
-	var area = d3.area()
-		.x(function(d) { return x(d.date); })
-		.y1(function(d) { return y(d.close); });
+	var yScale = d3.scale.linear()
+		.domain([0, d3.max(dataset, function (d) {
+			return d.y;
+		})])
+		.range([height, 0]);
 
-	d3.tsv("/img/data.tsv", function(d) {
-		d.date = parseTime(d.date);
-		d.close = +d.close;
-		return d;
-	}, function(error, data) {
-		if (error) throw error;
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom")
+		.innerTickSize(-height)
+		.outerTickSize(0)
+		.tickPadding(10);
 
-		x.domain(d3.extent(data, function(d) { return d.date; }));
-		y.domain([0, d3.max(data, function(d) { return d.close; })]);
-		area.y0(y(0));
+	var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left")
+		.innerTickSize(-width)
+		.outerTickSize(0)
+		.tickPadding(10);
 
-		g.append("path")
-			.datum(data)
-			.attr("fill", "steelblue")
-			.attr("d", area);
+	var line = d3.svg.line()
+		.x(function (d) {
+			return xScale(d.x);
+		})
+		.y(function (d) {
+			return yScale(d.y);
+		});
 
-		g.append("g")
-			.attr("transform", "translate(0," + height + ")")
-			.call(d3.axisBottom(x));
+	var area = d3.svg.area()
+		.x(function (d) {
+			return x(d.date);
+		})
+		.y1(function (d) {
+			return y(d.close);
+		});
 
-		g.append("g")
-			.call(d3.axisLeft(y))
-			.append("text")
-			.attr("fill", "#000")
-			.attr("transform", "rotate(-90)")
-			.attr("y", 6)
-			.attr("dy", "0.71em")
-			.attr("text-anchor", "end")
-			.text("Price ($)");
-	});
+	var svg = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");// устанавливает позицию отображения свг
+
+
+	var svgDefs = svg.append('defs');
+	var mainGradient = svgDefs.append('linearGradient')
+		.attr('id', 'mainGradient')
+		.attr('x1', '0')
+		.attr('x2', '0')
+		.attr('y1', '0')
+		.attr('y2', '1');
+
+	mainGradient.append('stop')
+		.attr('class', 'stop-left')
+		.attr('offset', '0');
+	mainGradient.append('stop')
+		.attr('class', 'stop-right')
+		.attr('offset', '1');
+
+
+	// вертикальные линии сетки
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+	// горизонтальные линии сетки
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis);
+
+	// график
+	svg.append("path")
+		.data([dataset])
+		.attr("class", "filled")
+		.attr("fill", "#000")
+		.attr("d", line);
 
 })();
+
